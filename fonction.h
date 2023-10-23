e05d6bfdfbe57ac38260033b215d856a4e6c433b
LucasGuiKtrn <lucas.guillen@edu.ece.fr>
2023-10-23 12:26:10 +0200

    Update fonction.h
    jjj

--- fonction.h
+++ fonction.h
@@ -1,19 +1,29 @@
 #include <stdio.h>
 
-void menuJeu()
+void menuJeu() {
+    puts("---LA REVANCHE DE SNOOPY---\n");
+
+    puts("---Menu du Jeu---");
+    puts("1) Regle du jeu");
+    puts("2) Lancer un nouveau jeu à partir du niveau 1");
+    puts("3) Charger une partie");
+    puts("4) Mot de passe");
+    puts("5) Scores");
+    puts("6) Quitter");
+
+}
+
+
+void regle_du_jeu()
+{
+    puts ("le joueur possède 3 vies. Chaque niveau devra être résolu en moins de 120 secondes. Si le temps est écoulé, le joueur perd une\n"
+          "vie et recommence le niveau. Le but est de récupérer les 4 oiseaux du niveau sans se faire toucher par\n"
+          "la balle et/ou les ennemis (si présents).");
+}
+
+void choix ()
 {
     int n;
-    puts ("---LA REVANCHE DE SNOOPY---\n");
-
-    puts ("---Menu du Jeu---");
-    puts ("1) Regle du jeu");
-    puts ("2) Lancer un nouveau jeu à partir du niveau 1");
-    puts ("3) Charger une partie");
-    puts ("4) Mot de passe");
-    puts ("5) Scores");
-    puts ("6) Quitter");
-
-
     puts ("Veuillez choisir l'un des menus du menu du Jeu ");
     scanf("%d", &n);
 
@@ -22,12 +32,7 @@ void menuJeu()
         puts ("Votre entier n'est pas compris entre 1 et 6. Veuillez choisir l'un des menus du menu du Jeu ");
         scanf("%d", &n);
     }
+
 }
 
-void regle_du_jeu()
-{
-    puts ("le joueur possède 3 vies. Chaque niveau devra être résolu en moins de 120 secondes. Si le temps est écoulé, le joueur perd une\n"
-          "vie et recommence le niveau. Le but est de récupérer les 4 oiseaux du niveau sans se faire toucher par\n"
-          "la balle et/ou les ennemis (si présents).");
-}
 
