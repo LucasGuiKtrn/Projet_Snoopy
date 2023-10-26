a3c13ab9091cf1b1d7df03b63f83db945b44474d
LucasGuiKtrn <lucas.guillen@edu.ece.fr>
2023-10-26 22:32:07 +0200

    Mise à jour

--- fonction.h
+++ fonction.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void menuJeu() {
     puts("---LA REVANCHE DE SNOOPY---\n");
@@ -35,23 +36,61 @@ void choix ()
 
 }
 
+void affichage (char map [10][20])
+{
+    int i, j;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 20; j++)
+        {
+            printf ("%d", map[i][j]);
+        }
+        printf ("\n");
+    }
+}
+
+
+//Sous - Programme qui permet de récuperer le fichier texte où il y a la première carte du jeu
+// Le fichier texte renvoie la carte en chaine de caractère, j'ai donc converti cette chaine de caractère en entier que j'ai mis dans un tableau une dimension pour ensuite transformer ce tableau
+//en un tableau 10 par 20 comme demandé
+
 void map1 (char map [10][20])
 {
+    int i = 0, j = 0, k = 0;
+    char chaine[209]; // j'ai mis 209 car vu que le fichier renvoie une chaine de caractère, les retour à la ligne compte comme un caractère, j'ai donc converti en entier pour enlever les retour à la ligne
+                      // De plus, c'est plus simple pour faire la suite
     FILE* fichier;
 
     fichier = fopen("carte1.txt", "r");
 
     if (fichier != NULL)
     {
-        int c;
-        while ((c = fgetc(fichier)) != EOF)
+        char c;
+        while ((c = (char) fgetc(fichier)) != EOF)
         {
-            printf ("%c", c);
+            if (c != '\n')
+            {
+                chaine[i] = atoi(&c);
+                i++;
+            }
         }
         fclose(fichier);
     }
     else
     {
-        printf ("Il y a un probleme");
+        printf ("Il y a un probleme avec le fichier de la carte du jeu");
     }
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 20; j++)
+        {
+            map[i][j] = chaine [k];
+            k++;
+        }
+    }
+
 }
+
+
--- main.c
+++ main.c
@@ -5,7 +5,9 @@
 int main ()
 {
     char carte[10][20];
+
     map1 (carte);
+    affichage(carte);
 
     return 0;
 }
